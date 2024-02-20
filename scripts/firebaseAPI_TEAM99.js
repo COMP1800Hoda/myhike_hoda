7a82df5b2f9272c42e70fa226de3990654a93d66
HodaRashedi <hrashedi1@bcit.ca>
2024-02-20 10:24:03 -0800

    demo07 completed

--- scripts/firebaseAPI_TEAM99.js
+++ scripts/firebaseAPI_TEAM99.js
@@ -2,12 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyAFH0umoBXsvaisqI8G5ZZ0qdjr_gILKtY",
+    authDomain: "comp1800-202410-demo-d5a94.firebaseapp.com",
+    projectId: "comp1800-202410-demo-d5a94",
+    storageBucket: "comp1800-202410-demo-d5a94.appspot.com",
+    messagingSenderId: "768403008693",
+    appId: "1:768403008693:web:deca399b0139128415e2fc"
 };
 
 //--------------------------------------------
